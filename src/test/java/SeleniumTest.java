11fa56986c7c5472683660caf8faca8735992bad
Flavio Ramos <flavio.ramos@usach.cl>
2021-11-09 17:34:36 -0300

    dependencies fix (?

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,7 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumTest {
+    @Test
     public static void main(String[] args) {
-
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("test@mail.cl");
+        WebElement password = driver.findElement(By.id("passwd"));
+        password.sendKeys("");
     }
 }
